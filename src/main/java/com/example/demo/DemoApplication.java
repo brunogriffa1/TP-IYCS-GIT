5f66424e82bf0f3c1a2c70716a26b4366654bbd4
FacuPerron <83370047+FacuPerron@users.noreply.github.com>
2026-09-17 19:32:04 -0300

    Modificación del archivo DemoApplicationJava

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,11 @@
 package com.example.demo; 
 
-import com.example.demo.repositorios.HabitacionRepositorio;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.example.demo.repositorios.HabitacionRepositorio;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -25,7 +26,8 @@ public class DemoApplication {
                 // Intentamos listar las habitaciones
                 // Si esto no da error, la conexión es EXITOSA
                 System.out.println("Consultando habitaciones...");
-                habitacionRepositorio.findAll(); 
+                System.out.println("¡Que tenga un gran dia!");
+                habitacionRepositorio.findAll();
                 
                 System.out.println(" ¡CONEXION EXITOSA! La base de datos responde.");
             } catch (Exception e) {
