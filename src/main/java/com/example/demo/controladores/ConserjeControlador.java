13bc3cd26a34d6e4ab06fece8160737e1bf8f574
MachoNaldo <igdmaldonado@gmail.com>
2026-09-18 17:47:37 -0300

    Correccion de errores

--- src/main/java/com/example/demo/controladores/ConserjeControlador.java
+++ src/main/java/com/example/demo/controladores/ConserjeControlador.java
@@ -1,16 +1,22 @@
 package com.example.demo.controladores;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.excepciones.EntidadNoEncontradaException;
 import com.example.demo.excepciones.ValidacionException;
 import com.example.demo.modelo.Conserje;
 import com.example.demo.servicios.ConserjeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import com.example.demo.excepciones.EntidadNoEncontradaException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/conserjes")
